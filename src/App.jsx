3d5b0a3f6dfe8bb1ba5d6eee7c87eba3c65e04b2
manucmadhu <125571706+manucmadhu@users.noreply.github.com>
2024-01-05 20:32:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Manu C Madhu",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "manucmadhu186@gmail.com",
+  gitHub: "manucmadhu",
+  instagram: "__.m_.a_.n_.u",
+  linkedIn: "manucmadhu",
 };
 
 const primaryColor = "#4E567E";
